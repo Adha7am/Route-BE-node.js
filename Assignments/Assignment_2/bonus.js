f5a860623d2f5c14cb9065b7bde78798fd573e24
Adham <Adhamwael047w@gmail.com>
2026-08-14 14:09:11 +0300

    fixed bonus.js

--- Assignments/Assignment_2/bonus.js
+++ Assignments/Assignment_2/bonus.js
@@ -1,8 +1,47 @@
 const { log } = require("console");
 
-const myarr = [2,3,5,7,11,12]
-const nnn = [1,2,3,4,5,6,7,8,9] // ==> 1,5,6,8
+const arr = [2,3,4,7]
 const k = 5;
+
+
+let max = 0;
+arr.map(
+    (n) => {
+        if(n > max){
+            max = n;
+        }     
+    }
+)
+
+console.log("max : " +max);
+
+let arrpos = [];
+for (let i = 0; i < max; i++) {         
+    arrpos[i] = max - i
+}
+
+console.log("positive numbers to check : "+ arrpos);
+
+
+arr.forEach(
+    (n, i) => {
+        if(arrpos.includes(n))
+        {
+            arrpos.forEach(
+                (m, j) => {
+                    if(m === n){    
+                        arrpos.splice(j,1)
+                    }
+                }
+            )   
+            
+        }
+    }
+)
+
+console.log(arrpos.reverse())
+
+//----------------------------------------------------------------------------
 
 // let arrpos = [];
 // let ontrack;
@@ -32,73 +71,3 @@ const k = 5;
 
 
 //----------------------------------------------------------------------------
-// function findKthPositive(arr, k){
-//     let lo = 0
-//     let hi = myarr.length;
-//     while (lo < hi){
-
-//         mid = (lo + hi) // 2
-//         missing = arr[mid] - (mid + 1)
-
-//         if (missing < k){
-//             lo = mid + 1
-//         }else{
-//             hi = mid
-//         }
-//     }
-//      return lo + k
-        
-// }
-// console.log(
-
-//     findKthPositive(myarr, k)
-// );
-
-//----------------------------------------------------------------------------
-
-
-
-// let max = 0;
-// arr.map(
-//     (n) => {
-//         if(n > max){
-//             max = n;
-//         }     
-//     }
-// )
-
-// console.log("max : "+max);
-
-// let arrpos = [];
-// for (let i = 0; i < max; i++) {         
-//     arrpos[i] = max - i
-// }
-
-// console.log("positive numbers to check : "+arrpos);
-
-
-// arr.forEach(
-//     (n, i) => {
-//         if(arrpos.includes(n))
-//         {
-//             arrpos.forEach(
-//                 (m, j) => {
-//                     if(m === n){    
-//                         arrpos.splice(j,1)
-//                     }
-//                 }
-//             )   
-            
-//         }
-//     }
-// )
-
-// console.log(arrpos.reverse())
-// console.log(arrpos[k-1]);
-
-
-
-// function fn(arr, k){
-
-// };
-
